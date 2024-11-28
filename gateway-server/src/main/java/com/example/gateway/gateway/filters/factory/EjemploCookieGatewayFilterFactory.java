2f9e01d6411bda5aebc3f9ed52c181af6689b481
Cristian <cristiananzawa@gmail.com>
2024-11-28 16:34:03 -0300

    gateway security config and application.yml

---
+++ gateway-server/src/main/java/com/example/gateway/gateway/filters/factory/EjemploCookieGatewayFilterFactory.java
@@ -0,0 +1,2 @@
+package com.example.gateway.gateway.filters.factory;public class EjemploCookieGatewayFilterFactory {
+}
---
+++ gateway-server/src/main/java/com/example/gateway/gateway/security/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.gateway.gateway.security;public class SecurityConfig {
+}
