8a94064972c9a6f8e2acc0fc51aa88289784a0e2
Cristian <cristiananzawa@gmail.com>
2024-12-26 17:42:15 -0300

    disable tracing with zipkin

---
+++ zipkin/zipkin.cmd
@@ -0,0 +1,6 @@
+@echo off
+set STORAGE_TYPE=postgresql
+set POSTGRES_USER=zipkin
+set POSTGRES_PASS=zipkin
+set POSTGRES_DB=zipkin
+java -jar zipkin-server-3.4.1-exec.jar
---
+++ zipkin/zipkin.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+STORAGE_TYPE=postgresql POSTGRES_USER=zipkin POSTGRES_PASS=zipkin java -jar zipkin-server-3.4.1-exec.jar
